c035d9e1f8ae2254d051a3d62132419ed2fbb80e
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2021-11-30 14:14:06 +0200

    Updates

---
+++ static/js/main.122c5447.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-start"]=this["webpackJsonpreact-start"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var r=n(1),c=n.n(r),s=n(3),a=n.n(s),i=n(0);var o=function(){return Object(i.jsx)("div",{})};n(9);a.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.122c5447.chunk.js.map
